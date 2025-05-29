e4f48f2e39105a24007568f9f9cb312857e68b38
Ajay <139728870+Ajay110702@users.noreply.github.com>
2025-05-29 11:45:43 +0530

    Update config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://localhost:4000/api/v1'
+export const BASE_URL = 'https://food-del-backend-8c27.onrender.com'
 // export const BASE_URL = 'https://travelweb-backend-uns3.onrender.com'
 // export const BASE_URL = 'https://travelweb-backend.onrender.com'  
