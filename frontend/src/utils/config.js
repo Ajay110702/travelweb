bff424761b4d9a22e6547d5f87f9e64edfbb5982
Ajay <ajayr5096@gmail.com>
2025-05-29 11:47:06 +0530

    commit

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,4 @@
-export const BASE_URL = 'http://localhost:4000/api/v1'
+// export const BASE_URL = 'http://localhost:4000/api/v1'
+export const BASE_URL = 'https://food-del-backend-8c27.onrender.com'
 // export const BASE_URL = 'https://travelweb-backend-uns3.onrender.com'
 // export const BASE_URL = 'https://travelweb-backend.onrender.com'  
